63a1b06127633020485381fe0a3fa6ed39cdd1f7
Vera <ladog-vera@mail.ru>
2022-09-19 20:34:08 +0700

    добавила project01

---
+++ Project01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Hi, ");
+Console.WriteLine(username);
+Console.Write("Введите ваше имя");
+string username1 = Console.ReadLine();
+Console.Write("Hi, ");
+Console.Write(username1);
